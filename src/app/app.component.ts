32a3edac78703313d98acf91e32f50f0ce94e1eb
PooraniGunasekaran <poorani.gunasekaran-extern@rntbci.com>
2023-04-03 14:45:27 +0530

    remove wrong swiper code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,10 @@
 import { Component } from '@angular/core';
-import { SwiperOptions } from 'swiper';
 
-// import Swiper core and required components
-import SwiperCore , {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-  Virtual,
-  Zoom,
-  Autoplay,
-  Thumbs,
-  Controller,
-} from 'swiper';
-// install Swiper components
-SwiperCore.use([
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-  Virtual,
-  Zoom,
-  Autoplay,
-  Thumbs,
-  Controller
-]);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  config: SwiperOptions = {
-    pagination: { 
-      el: '.swiper-pagination', 
-      clickable: true 
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
-    spaceBetween: 30
-  };  
+   
 }
