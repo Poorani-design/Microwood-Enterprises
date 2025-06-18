332fc64e519fed3142fd9f382f737b2bf6e7f351
Poorani-design <94600498+Poorani-design@users.noreply.github.com>
2025-06-18 20:59:43 +0530

    final commit

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -24,24 +24,37 @@ export class ProductComponent {
     1200:{ slidesPerView: 5, spaceBetween: 20 }
   };
   images: any;
+  clientImages: any;
   
   ngOnInit(){
     this.images =[
       // '../../assets/images/product-slider-compressed/microwood_enterprises_1-min.jpg',
       // '../../assets/images/product-slider-compressed/microwood_enterprises_2-min.jpg',
       // '../../assets/images/product-slider-compressed/microwood_enterprises_3-min.jpg',
-      // '../../assets/images/product-slider-compressed/microwood_enterprises_4-min.jpg',
+  
       '../../assets/images/product-slider-compressed/microwood_enterprises_5-min.jpg',
       '../../assets/images/product-slider-compressed/microwood_enterprises_6-min.jpg',
       '../../assets/images/product-slider-compressed/microwood_enterprises_7-min.jpg',
       '../../assets/images/product-slider-compressed/microwood_enterprises_8-min.jpg',
       '../../assets/images/product-slider-compressed/microwood_enterprises_9-min.jpg',
-      '../../assets/images/product-slider-compressed/microwood_enterprises_10-min.jpg',
+      // '../../assets/images/product-slider-compressed/microwood_enterprises_10-min.jpg',
       // '../../assets/images/product-slider-compressed/microwood_enterprises_11-min.jpg',
       // '../../assets/images/product-slider-compressed/microwood_enterprises_12-min.jpg',
       // '../../assets/images/product-slider-compressed/microwood_enterprises_13-min.jpg',
       // '../../assets/images/product-slider-compressed/microwood_enterprises_14-min.jpg'
   
+    ]
+    this.clientImages=[
+      '../../assets/images/customer-projects-slider/microwood_enterprises_customer_proejct_1.jpg',
+      '../../assets/images/customer-projects-slider/microwood_enterprises_customer_proejct_2.jpg',
+      '../../assets/images/customer-projects-slider/microwood_enterprises_customer_proejct_3.jpg',
+      '../../assets/images/customer-projects-slider/microwood_enterprises_customer_proejct_4.jpg',
+      '../../assets/images/customer-projects-slider/microwood_enterprises_customer_proejct_5.jpg',
+      '../../assets/images/customer-projects-slider/microwood_enterprises_customer_proejct_6.jpg',
+      '../../assets/images/customer-projects-slider/microwood_enterprises_customer_proejct_7.jpg',
+      '../../assets/images/customer-projects-slider/microwood_enterprises_customer_proejct_8.jpg',
+      '../../assets/images/customer-projects-slider/microwood_enterprises_customer_proejct_9.jpg',
+      '../../assets/images/customer-projects-slider/microwood_enterprises_customer_proejct_10.jpg',
     ]
   }
 }
